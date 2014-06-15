d4246491bd2223158146faceaaa7ceabb6ef4872
scotchfield <swrittenb@gmail.com>
2014-06-14 22:01:41 -0400

    Fixing whitespace between identifiers, parentheses, and so on..
    I'm whitespace crazy now.  Argh, standards!  Can't help it.  It just looks
    so clean!

--- royale.py
+++ royale.py
@@ -4,7 +4,7 @@ from world import World
 
 VERBOSE = True
 
-AGENT_CHARS = ['!', '@', '$', '%', '^', '&', '(', ')']
+AGENT_CHARS = [ '!', '@', '$', '%', '^', '&', '(', ')' ]
 
 AGENT_COMMANDS = [
     'pass',
@@ -14,8 +14,8 @@ AGENT_COMMANDS = [
 
 def main():
     w = World()
-    w.randomize(20, 12, 30)
-    print(w)
+    w.randomize( 20, 12, 30 )
+    print( w )
 
     player_obj = [
         StumbleAgent(),
@@ -24,8 +24,8 @@ def main():
     ]
 
     state_obj = []
-    for i in range(len(player_obj)):
-        (x, y) = w.get_empty()
+    for i in range( len( player_obj ) ):
+        ( x, y ) = w.get_empty()
         state_obj.append(
             {
                 'health': 100,            # 
@@ -38,7 +38,7 @@ def main():
                 'x': x,                   # 
                 'y': y,                   # 
                 'inventory': [],          # 
-                'c': random.choice(AGENT_CHARS),
+                'c': random.choice( AGENT_CHARS ),
             }
         )
 
@@ -46,71 +46,76 @@ def main():
     done = False
     while not done:
         if VERBOSE:
-            print('# Cycle {0} #'.format(cycles_left))
+            print( '# Cycle {0} #'.format( cycles_left ) )
 
         w.reset_characters()
-        for i in range(len(player_obj)):
-            w.add_character(state_obj[i]['x'], state_obj[i]['y'],
-                            state_obj[i])
+        for i in range( len( player_obj ) ):
+            w.add_character( state_obj[ i ][ 'x' ], state_obj[ i ][ 'y' ],
+                             state_obj[ i ] )
             # TODO obscure some of this state
-            print(state_obj[i])
+            print( state_obj[ i ] )
 
-        print(w)
+        print( w )
 
         action_obj = []
-        for i in range(len(player_obj)):
+        for i in range( len( player_obj ) ):
             state = {
                 'agent': {
-                    'health': state_obj[i]['health'],
-                    'weapon_name': state_obj[i]['weapon_name'],
-                    'weapon_damage_n': state_obj[i]['weapon_damage_n'],
-                    'weapon_damage_d': state_obj[i]['weapon_damage_d'],
+                    'health': state_obj[ i ][ 'health' ],
+                    'weapon_name': state_obj[ i ][ 'weapon_name' ],
+                    'weapon_damage_n': state_obj[ i ][ 'weapon_damage_n' ],
+                    'weapon_damage_d': state_obj[ i ][ 'weapon_damage_d' ],
                 },
-                'view': w.get_local(state_obj[i]['x'], state_obj[i]['y'],
-                                    state_obj[i]['visibility']),
+                'view': w.get_local( state_obj[ i ][ 'x' ],
+                                     state_obj[ i ][ 'y' ],
+                                     state_obj[ i ][ 'visibility' ] ),
             }
             
-            action = player_obj[i].update(state)
-            action_obj.append((i, action, state_obj[i]['initiative']))
+            action = player_obj[ i ].update( state )
+            action_obj.append( ( i, action, state_obj[ i ][ 'initiative' ] ) )
             if VERBOSE:
-                print('\tplayer {0} ({1}): {2}'.format(
-                        i, str(player_obj[i]), action))
+                print( '\tplayer {0} ({1}): {2}'.format(
+                          i, str( player_obj[ i ] ), action ) )
 
         # first we need to process actions
         #   for example, attack another player based on their position
         # once that's done, we make appropriate changes to the state
         state_change_obj = []
-        for i in range(len(player_obj)):
-            state_change_obj.append({})
+        for i in range( len( player_obj ) ):
+            state_change_obj.append( {} )
 
-        random.shuffle(action_obj) # TODO consider initiative
+        random.shuffle( action_obj ) # TODO consider initiative
         for action in action_obj:
-            p = action[0]
-            a = action[1].split(' ')
+            p = action[ 0 ]
+            a = action[ 1 ].split(' ')
 
-            if a[0] not in AGENT_COMMANDS:
-                print('WARNING: {0}'.format(action))
+            if a[ 0 ] not in AGENT_COMMANDS:
+                print( 'WARNING: {0}'.format( action ) )
                 continue
 
-            if a[0] == 'pass':
+            if a[ 0 ] == 'pass':
                 pass
-            elif a[0] == 'north':
-                if w.is_empty(state_obj[p]['x'], state_obj[p]['y'] - 1):
-                    state_change_obj[p]['y'] = state_obj[p]['y'] - 1
-            elif a[0] == 'south':
-                if w.is_empty(state_obj[p]['x'], state_obj[p]['y'] + 1):
-                    state_change_obj[p]['y'] = state_obj[p]['y'] + 1
-            elif a[0] == 'east':
-                if w.is_empty(state_obj[p]['x'] + 1, state_obj[p]['y']):
-                    state_change_obj[p]['x'] = state_obj[p]['x'] + 1
-            elif a[0] == 'west':
-                if w.is_empty(state_obj[p]['x'] - 1, state_obj[p]['y']):
-                    state_change_obj[p]['x'] = state_obj[p]['x'] - 1
+            elif a[ 0 ] == 'north':
+                if w.is_empty( state_obj[ p ][ 'x' ],
+                               state_obj[ p ][ 'y' ] - 1 ):
+                    state_change_obj[ p ][ 'y' ] = state_obj[ p ][ 'y' ] - 1
+            elif a[ 0 ] == 'south':
+                if w.is_empty( state_obj[ p ][ 'x' ],
+                               state_obj[ p ][ 'y' ] + 1 ):
+                    state_change_obj[ p ][ 'y' ] = state_obj[ p ][ 'y' ] + 1
+            elif a[ 0 ] == 'east':
+                if w.is_empty( state_obj[ p ][ 'x' ] + 1,
+                               state_obj[ p ][ 'y' ] ):
+                    state_change_obj[ p ][ 'x' ] = state_obj[ p ][ 'x' ] + 1
+            elif a[ 0 ] == 'west':
+                if w.is_empty( state_obj[ p ][ 'x' ] - 1,
+                               state_obj[ p ][ 'y' ] ):
+                    state_change_obj[ p ][ 'x' ] = state_obj[ p ][ 'x' ] - 1
             #print(action)
 
-        for i in range(len(player_obj)):
-            for k in state_change_obj[i].keys():
-                state_obj[i][k] = state_change_obj[i][k]
+        for i in range( len( player_obj ) ):
+            for k in state_change_obj[ i ].keys():
+                state_obj[ i ][ k ] = state_change_obj[ i ][ k ]
 
         cycles_left -= 1
         if cycles_left <= 0:
@@ -119,25 +124,24 @@ def main():
 
 
 class AbstractAgent():
-    def __str__(self):
+    def __str__( self ):
         return 'AbstractAgent'
-    def update(self, state):
-        return ('pass')
+    def update( self, state ):
+        return ( 'pass' )
 
-class PassAgent(AbstractAgent):
-    def __str__(self):
+class PassAgent( AbstractAgent ):
+    def __str__( self ):
         return 'PassAgent'
-    def update(self, state):
-        #print(state)
-        return ('pass')
+    def update( self, state ):
+        return ( 'pass' )
 
-class StumbleAgent(AbstractAgent):
-    def __str__(self):
+class StumbleAgent( AbstractAgent ):
+    def __str__( self ):
         return 'StumbleAgent'
-    def update(self, state):
-        print(state['view'])
-        actions = ['north', 'south', 'east', 'west']
-        return random.choice(actions)
+    def update( self, state ):
+        print( state[ 'view' ] )
+        actions = [ 'north', 'south', 'east', 'west' ]
+        return random.choice( actions )
 
 if __name__ == "__main__":
     main()
--- world.py
+++ world.py
@@ -1,10 +1,10 @@
 import random
 
-ITEM_T = ['weapon']
+ITEM_T = [ 'weapon' ]
 
-def item(n, t, dmg_n, dmg_d, dist) :
+def item( n, t, dmg_n, dmg_d, dist ):
     if t not in ITEM_T:
-        print('WARNING ({0}): {1} not valid type'.format(n, t))
+        print( 'WARNING ({0}): {1} not valid type'.format( n, t ) )
     return {
         'name': n,
         'type': t,
@@ -14,45 +14,44 @@ def item(n, t, dmg_n, dmg_d, dist) :
     }
 
 ITEM_OBJ = {
-    'pistol': item('Pistol', 'weapon', 3, 20, 8),
-
+    'pistol': item( 'Pistol', 'weapon', 3, 20, 8 ),
 }
 
 class World():
 
-    def __init__(self):
+    def __init__( self ):
         self.__tiles = []
         self.__items = []
         self.__characters = [] # for blocking, not trusted data
 
-    def __str__(self):
+    def __str__( self ):
         st = ''
-        for y in range(len(self.__tiles)):
-            for x in range(len(self.__tiles[y])):
-                t = self.is_character_at(x, y)
+        for y in range( len( self.__tiles ) ):
+            for x in range( len( self.__tiles[ y ] ) ):
+                t = self.is_character_at( x, y )
                 if t:
-                    st = '{0}{1}'.format(st, t[2]['c'])
+                    st = '{0}{1}'.format( st, t[ 2 ][ 'c' ] )
                     continue
-                t = self.is_item_at(x, y)
+                t = self.is_item_at( x, y )
                 if t:
-                    st = '{0}{1}'.format(st, '*')
+                    st = '{0}{1}'.format( st, '*' )
                     continue
-                st = '{0}{1}'.format(st, self.get(x, y))
-            st = '{0}\n'.format(st)
-        st = '{0}{1}'.format(st, self.__items)
+                st = '{0}{1}'.format( st, self.get( x, y ) )
+            st = '{0}\n'.format( st )
+        st = '{0}{1}'.format( st, self.__items )
         return st
 
-    def randomize(self, x, y, n):
+    def randomize( self, x, y, n ):
         self.__x = x
         self.__y = y
-        for i in range(y):
-            self.__tiles.append(['.'] * x)
-        for i in range(n):
+        for i in range( y ):
+            self.__tiles.append( [ '.' ] * x )
+        for i in range( n ):
             self.__items.append(
-                (random.randint(0, x), random.randint(0, y),
-                 random.choice(ITEM_OBJ.keys())))
+                ( random.randint( 0, x ), random.randint( 0, y ),
+                  random.choice( ITEM_OBJ.keys() ) ) )
 
-    def copy(self, w, x, y, visibility):
+    def copy( self, w, x, y, visibility ):
         size = visibility * 2 + 1
         self.__x = size
         self.__y = size
@@ -61,74 +60,74 @@ class World():
         x_max = x + visibility
         y_min = y - visibility
         y_max = y + visibility
-        for j in range(size):
+        for j in range( size ):
             row = []
-            for i in range(size):
-                row.append(w.get(i + x_min, j + y_min))
-            self.__tiles.append(row)
+            for i in range( size ):
+                row.append( w.get( i + x_min, j + y_min ) )
+            self.__tiles.append( row )
         for t in w.__items:
-            if (t[0] >= x_min) and (t[0] <= x_max) and (t[1] >= y_min) and (t[1] <= y_max):
-                print(t)
-                print(x, y, visibility)
-                self.__items.append(t) # TODO NOT CORRECT LOCATION IN REMAPPING
+            if ( t[ 0 ] >= x_min ) and ( t[ 0 ] <= x_max ) and ( t[ 1 ] >= y_min ) and ( t[ 1 ] <= y_max ):
+                print( t )
+                print( x, y, visibility )
+                self.__items.append( t ) # TODO NOT CORRECT LOCATION IN REMAPPING
         for t in w.__characters:
-            print(t)
-            if (t[0] >= x_min) and (t[0] <= x_max) and (t[1] >= y_min) and (t[1] <= y_max):
-                self.__characters.append(t) # TODO NOT CORRECT LOCATION IN REMAPPING
+            print( t )
+            if ( t[ 0 ] >= x_min ) and ( t[ 0 ] <= x_max ) and ( t[ 1 ] >= y_min ) and ( t[ 1 ] <= y_max ):
+                self.__characters.append( t ) # TODO NOT CORRECT LOCATION IN REMAPPING
 
-    def get(self, x, y):
-        if y >= 0 and y < len(self.__tiles):
-            if x >= 0 and x < len(self.__tiles[y]):
-                return self.__tiles[y][x]
+    def get( self, x, y ):
+        if y >= 0 and y < len( self.__tiles ):
+            if x >= 0 and x < len( self.__tiles[ y ] ):
+                return self.__tiles[ y ][ x ]
         return '#'
 
-    def get_local(self, x, y, visibility):
+    def get_local( self, x, y, visibility ):
         w = World()
-        w.copy(self, x, y, visibility)
+        w.copy( self, x, y, visibility )
         return w
 
-    def is_blocked(self, x, y):
+    def is_blocked( self, x, y ):
         if x < 0 or x >= self.__x:
             return True
         if y < 0 or y >= self.__y:
             return True
-        if self.get(x, y) in ['#']:
+        if self.get( x, y ) in [ '#' ]:
             return True
         return False
 
-    def is_empty(self, x, y):
-        if self.is_blocked(x, y):
+    def is_empty( self, x, y ):
+        if self.is_blocked( x, y ):
             return False
         for t in self.__items:
-            if t[0] == x and t[1] == y:
+            if t[ 0 ] == x and t[ 1 ] == y:
                 return False
         for t in self.__characters:
-            if t[0] == x and t[1] == y:
+            if t[ 0 ] == x and t[ 1 ] == y:
                 return False
         return True
 
-    def get_empty(self):
+    def get_empty( self ):
         done = False
         while True: # TODO this could be infinite, cut off after some point
-            x = random.randint(0, self.__x)
-            y = random.randint(0, self.__y)
-            if self.is_empty(x, y):
-                return (x, y)
+            x = random.randint( 0, self.__x )
+            y = random.randint( 0, self.__y )
+            if self.is_empty( x, y ):
+                return ( x, y )
             
-    def add_character(self, x, y, state): # TODO not sufficient now, need state
-        self.__characters.append((x, y, state))
+    def add_character( self, x, y, state ): # TODO not sufficient now, need state
+        self.__characters.append( ( x, y, state ) )
 
-    def reset_characters(self):
+    def reset_characters( self ):
         self.__characters = []
 
-    def is_item_at(self, x, y):
+    def is_item_at( self, x, y ):
         for t in self.__items:
-            if t[0] == x and t[1] == y:
+            if t[ 0 ] == x and t[ 1 ] == y:
                 return t
         return False
 
-    def is_character_at(self, x, y):
+    def is_character_at( self, x, y ):
         for t in self.__characters:
-            if t[0] == x and t[1] == y:
+            if t[ 0 ] == x and t[ 1 ] == y:
                 return t
         return False
